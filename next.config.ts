b81281d57391961d037cc5f978ee67ccba2173db
Phucpham <phamthanhqb2005@gmail.com>
2026-07-15 16:02:22 +0700

    feat(api): add digitalocean spaces upload api and configure next.config hosts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'hackathon-submissions.sgp1.digitaloceanspaces.com',
+        pathname: '/**',
+      },
     ],
   },
 };
